95760ddff900dfa15b0ca888d3ac27a90a736272
AmdouniBrahmiKhouloud <khouloud.AmdouniBrahmi@esprit.tn>
2021-05-16 09:36:57 +0100

    design with adding

---
+++ PIDev-mobile/src/com/mycompany/pidevapp/gui/FormAddOffre.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pidevapp.gui;
+
+import com.codename1.ui.Button;
+import com.codename1.ui.ComboBox;
+import com.codename1.ui.Command;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.PickerComponent;
+import com.codename1.ui.TextComponent;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.spinner.Picker;
+import com.codename1.ui.table.TableLayout;
+import com.codename1.ui.validation.LengthConstraint;
+import com.codename1.ui.validation.NumericConstraint;
+import com.codename1.ui.validation.Validator;
+import com.mycompany.pidevapp.entities.Contrat;
+import com.mycompany.pidevapp.entities.Offre;
+import com.mycompany.pidevapp.services.ServiceOffre;
+import java.util.Date;
+
+/**
+ *
+ * @author brahm
+ */
+public class FormAddOffre  extends BaseForm{
+     public FormAddOffre() {
+        setLayout(new com.codename1.ui.layouts.BoxLayout(com.codename1.ui.layouts.BoxLayout.Y_AXIS));
+        getToolbar().setTitleComponent(
+                      FlowLayout.encloseCenterMiddle(
+                                new Label("Ajouter une nouvelle offre", "Title")
+                        )
+                );
+        //setTitle("Ajouter une nouvelle offre");
+        TextComponent post = new TextComponent().label("Post");
+        TextComponent objectif = new TextComponent().label("Objectif");
+        TextComponent competence = new TextComponent().label("Competence");
+        String elements [] = {"Aéronautique Et Espace",
+                    "Agriculture - Agroalimentaire",
+                    "Artisanat",
+                    "Audiovisuel, Cinéma",
+                    "Audit, Comptabilité, Gestion",
+                    "Automobile",
+                    "Banque, Assurance",
+                    "Bâtiment, Travaux Publics",
+                    "Biologie, Chimie, Pharmacie",
+                    "Commerce, Distribution",
+                    "Communication",
+                    "Création, Métiers art",
+                    "Culture, Patrimoine",
+                    "Défense, Sécurité, Armée",
+                    "Documentation, Bibliothèque",
+                    "Droit",
+                    "Edition, Livre",
+                    "Enseignement",
+                    "Environnement",
+                    "Ferroviaire",
+                    "Foires, Salons Et Congrès",
+                    "Fonction Publique",
+                    "Hôtellerie, Restauration",
+                    "Humanitaire",
+                    "Immobilier",
+                    "Industrie",
+                    "Informatique, Télécoms, Web",
+                    "Jeu Vidéo",
+                    "Journalisme",
+                    "Langues",
+                    "Marketing, Publicité",
+                    "Médical",
+                    "Mode-Textile",
+                    "Paramédical",
+                    "Propreté Et Services Associés",
+                    "Psychologie",
+                    "Ressources Humaines",
+                    "Sciences Humaines Et Sociales",
+                    "Secrétariat",
+                    "Social",
+                    "Spectacle - Métiers De La Scène",
+                    "Sport",
+                    "Tourisme",
+                    "Transport-Logistique"};
+        ComboBox <String> cbnDomaine = new ComboBox();
+        for(int i=0; i<elements.length;i++){
+            cbnDomaine.addItem(elements[i]);
+        }
+        TextComponent description = new TextComponent().label("Description").multiline(true);
+        TextComponent salaire = new TextComponent().label("Salaire");
+        //PickerComponent dateExpiration = PickerComponent.createDate(new Date()).label("Date");
+        Picker dateExpiration = new Picker();
+        TextComponent nbPlace = new TextComponent().label("Nombre Place");
+        Label lbExpiration = new Label("Experience");
+        TextComponent min = new TextComponent().label("Min");
+        TextComponent max = new TextComponent().label("Max");
+        Container c = new Container(new TableLayout(2, 2));
+        c.add(min);
+        c.add(max);
+        Validator val = new Validator();
+        val.addConstraint(post, new LengthConstraint(2));
+        val.addConstraint(salaire, new NumericConstraint(true));
+        Button btn = new Button();
+        btn.setText("Next");
+        addAll(post,objectif,competence,cbnDomaine,description,salaire,dateExpiration,nbPlace,lbExpiration,c,btn);
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                if ((post.getText().length()==0)||(objectif.getText().length()==0))
+                    Dialog.show("Alert", "Please fill all the fields", new Command("OK"));
+                else
+                {
+                    try {
+                        Offre o = new Offre();
+                        o.setPost(post.getText());
+                        o.setObjectif(objectif.getText());
+                        o.setCompetences(competence.getText());
+                        o.setDomaine(cbnDomaine.getSelectedItem());
+                        o.setDescription(description.getText());
+                        o.setSalaire(Integer.parseInt(salaire.getText()));
+                        o.setDateExpiration(dateExpiration.getDate());
+                        o.setNombrePlace(Integer.parseInt(nbPlace.getText()));
+                        o.setExperienceMin(Integer.parseInt(min.getText()));
+                        o.setExperienceMax(Integer.parseInt(max.getText()));
+                        Contrat contrat = new Contrat();
+                        contrat.setId(1);
+                        o.setContrat(contrat);
+                        o.setDateDepot(new Date());
+                        System.out.println(o.toString());
+                        if( ServiceOffre.getInstance().addOffre(o))
+                            Dialog.show("Success","Connection accepted",new Command("OK"));
+                        else
+                            Dialog.show("ERROR", "Server error", new Command("OK"));
+                    } catch (NumberFormatException e) {
+                        Dialog.show("ERROR", "Status must be a number", new Command("OK"));
+                    }
+                    
+                }
+                //System.out.println(so.getAllOffres().toString());
+            }
+        });
+        
+    }
+
+//    @Override
+//    protected boolean isCurrentInbox() {
+//        return true;
+//    }
+
+
+}
--- PIDev-mobile/src/com/mycompany/pidevapp/services/ServiceOffre.java
+++ PIDev-mobile/src/com/mycompany/pidevapp/services/ServiceOffre.java
@@ -5,13 +5,23 @@
  */
 package com.mycompany.pidevapp.services;
 
+import com.codename1.io.CharArrayReader;
 import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
 import com.codename1.io.NetworkEvent;
 import com.codename1.io.NetworkManager;
+import com.codename1.l10n.DateFormatPatterns;
+import com.codename1.l10n.ParseException;
+import com.codename1.l10n.SimpleDateFormat;
 import com.codename1.ui.events.ActionListener;
 import com.mycompany.pidevapp.entities.Offre;
 import com.mycompany.pidevapp.utils.Statics;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  *
@@ -22,7 +32,7 @@ public class ServiceOffre {
     public static ServiceOffre instance=null;
     public boolean resultOK;
     private ConnectionRequest req;
-    private ServiceOffre() {
+    public ServiceOffre() {
          req = new ConnectionRequest();
     }
 
@@ -34,9 +44,11 @@ public class ServiceOffre {
     }
      public boolean addOffre(Offre o) {
          //création de l'URL
-         String url = Statics.BASE_URL +"/api/offre/new?description="+o.getDescription()
+         SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+         String date1 = format1.format(o.getDateExpiration());
+         String url = Statics.BASE_URL +"api/offre/new?description="+o.getDescription()
                  +"&salaire="+o.getSalaire()
-                 + "&dateExpiration="+o.getDateExpiration()
+                 + "&dateExpiration="+date1
                  + "&nombrePlace="+o.getNombrePlace()
                  + "&contrat="+o.getContrat().getId()
                  + "&post="+o.getPost()
@@ -57,4 +69,84 @@ public class ServiceOffre {
         NetworkManager.getInstance().addToQueueAndWait(req);
         return resultOK;
 }
+     public ArrayList<Offre> parseOffres(String jsonText) throws ParseException{
+        try {
+            offres=new ArrayList<>();
+            JSONParser j = new JSONParser();
+            Map<String,Object> offresListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            List<Map<String,Object>> list = (List<Map<String,Object>>)offresListJson.get("root");
+            for(Map<String,Object> obj : list){
+                Offre o = new Offre();
+                float id = Float.parseFloat(obj.get("id").toString());
+                String description = obj.get("description").toString();
+                float salaire = Float.parseFloat(obj.get("salaire").toString());
+//                String dateDepoString =  obj.get("dateDepo").toString();
+//                SimpleDateFormat format = new SimpleDateFormat(DateFormatPatterns.ISO8601);
+//                Date dateDepo = format.parse(dateDepoString );
+//                String dateExpirationString =  obj.get("dateExpiration").toString();
+//                Date dateExpiration = format.parse(dateExpirationString );
+                float nombrePlace = Float.parseFloat(obj.get("nombrePlace").toString());
+                //contrait
+                String post = obj.get("post").toString();
+                String objectif=obj.get("objectif").toString();
+                String competences=obj.get("competences").toString();
+                String domaine=obj.get("domaine").toString();
+                o.setId((int)id);
+                o.setDescription(description);
+                o.setSalaire((int)salaire);
+//                o.setDateDepot(dateDepo);
+//                o.setDateExpiration(dateExpiration);
+                o.setNombrePlace((int)nombrePlace);
+                o.setPost(post);
+                o.setObjectif(objectif);
+                o.setCompetences(competences);
+                o.setDomaine(domaine);
+//                t.setStatus(((int)Float.parseFloat(obj.get("status").toString())));
+//                t.setName(obj.get("name").toString());
+                //Ajouter la tâche extraite de la réponse Json à la liste
+                offres.add(o);
+            }
+            
+            
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return offres;
+    }
+     public ArrayList<Offre> getAllOffres(){
+        String url = Statics.BASE_URL+"api/offre";
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                try {
+                    offres = parseOffres(new String(req.getResponseData()));
+                } catch (ParseException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return offres;
+    }
+      public boolean deleteOffre (Offre o)
+     {
+         String url = Statics.BASE_URL+"offre/delete/"+o.getId();
+         req.setUrl(url);
+         req.addResponseListener(new ActionListener<NetworkEvent>() {
+             
+             @Override
+             public void actionPerformed(NetworkEvent evt){
+                 
+                 req.removeResponseCodeListener(this);
+             }
+             
+         });
+         
+         NetworkManager.getInstance().addToQueueAndWait(req);
+         return resultOK;
+     }
+     
 }
